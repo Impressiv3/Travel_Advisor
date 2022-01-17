9a2edf7998b23b02fa8532d22a68fd5085844a90
Infamou5 <oreal_b52@hotmail.com>
2022-01-17 09:31:18 +0100

    Implemented Lists

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -11,22 +11,17 @@ const Map = () => {
 
     const classes = useStyles();
     const isMobile = useMediaQuery('(min-width:600px)');
-
+    const coords  = { lat: 0, lng: 0}
 
     return (
         <div className={classes.mapContainer}>
             <GoogleMapReact
-             bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+             bootstrapURLKeys={{ key: "AIzaSyBcB2_JzVBXXCXZf7dY3V2ppmoUnqZx0cM" }}
              defaultCenter={coords}
              center={coords}
              defaultZoom={14}
              margin={[50, 50, 50, 50]}
-             options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
-             onChange={(e) => {
-               setCoords({ lat: e.center.lat, lng: e.center.lng });
-               setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
-             }}
-             onChildClick={(child) => setChildClicked(child)}
+             options={{ disableDefaultUI: true, zoomControl: true }}
            >
 
             </GoogleMapReact>
